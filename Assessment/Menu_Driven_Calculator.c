da6426bc4220da559948af6cb7d2c7ebbed9b982
priyank <priyankmali297@gmail.com>
2024-02-03 11:00:23 +0530

    answers

--- Assessment/Menu_Driven_Calculator.c
+++ Assessment/Menu_Driven_Calculator.c
@@ -15,46 +15,60 @@
  int main(){
     int choice;
     float a,b;
+    do{
     printf("---------------MENU---------------\n");
     printf("1. Addition\n");
     printf("2. Subtraction\n");
     printf("3. Multiplication\n");
     printf("4. Division\n");
 
-    if(choice<=4){
-        printf("\nEnter your choice: ");
-        scanf("%d",&choice);
+    printf("\nEnter your choice: ");
+    scanf("%d",&choice);
     
+    if(choice<5 ){
       switch(choice){
+        
         case 1: 
         printf("\nEnter first number: "); scanf("%f",&a);
         printf("Enter second number: "); scanf("%f",&b);
-        printf("\nAddition =  %f ",add(a,b));
+        printf("\nAddition =  %f \n",add(a,b));
         break;
 
         case 2: 
         printf("Enter first number: "); scanf("%f",&a);
         printf("Enter second number: "); scanf("%f",&b);
-        printf("\nSubtraction =  %f ",sub(a,b));
+        printf("\nSubtraction =  %f \n",sub(a,b));
         break;
 
         case 3: 
         printf("Enter first number: "); scanf("%f",&a);
         printf("Enter second number: "); scanf("%f",&b);
-        printf("\nMultiplication =  %f ",mul(a,b));
+        printf("\nMultiplication =  %f \n",mul(a,b)); 
         break;
 
         case 4: 
         printf("Enter first number: "); scanf("%f",&a);
         printf("Enter second number: "); scanf("%f",&b);
-        printf("\nDivision =  %f ",div(a,b));
-        break;
-        
-        default:
-        printf("Invalid choice !!!!");
-        }
+            if(b!=0){
+            printf("\nDivision =  %f \n",div(a,b)); 
+                }
+            else{
+            printf("ERROR : Division by zero not allowed\n");
+               }
+            break;
+    
+         }
     }
-        printf("\n---------------------------------");
-    return 0;
- }
+      else if(choice != 5){
+      printf("Invalid choice !!!\n");
+    }
+     } while(choice!=5);
+     printf("Calculator is closed\n");
+     printf("------------------------------");
+         
+   }
+ 
+ 
+    
+
  
